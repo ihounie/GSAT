9ec568a1dfa3ba6c1e6259409e1ced4966e79c24
ihounie <ihounie@gmail.com>
2024-03-18 12:52:34 -0400

    param fixes

--- experiments/sweep_alpha.sh
+++ experiments/sweep_alpha.sh
@@ -1,4 +1,4 @@
 for alpha in 0.1 1.0 10.0 100.0 1000.0 10000.0 0.01 0.001
 do
-    papermill resilience.ipynb resilience_alpha_${alpha}.ipynb -p alpha $alpha
+    papermill resilience.ipynb resilience_alpha_${alpha}.ipynb -p resilient_alpha $alpha
 done
